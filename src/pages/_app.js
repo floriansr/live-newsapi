27ac06ea380ab083ee8336f3430624b8f5a2bd01
floriansr <sueurfloriansf@gmail.com>
2020-12-11 18:42:24 +0100

    [change] ES6 imports

---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <h3>Hello world !</h3>
+    </>
+  );
+};
+
+export default Home;
