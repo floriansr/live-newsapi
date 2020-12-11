f7643acc6afcfe34cdfeefc371cb5e4216d736e1
floriansr <sueurfloriansf@gmail.com>
2020-12-11 19:20:20 +0100

    3 fetch newsapi (#2)
    * [add] axios packages
    * [create] api manager
    * [add] fetch some science news (works)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,17 @@
 import * as React from 'react';
+import { useEffect } from 'react';
+
+import APIManager from '../services/APIManager';
 
 const Home = () => {
+  useEffect(() => {
+    const fetch = async () => {
+      const res = await APIManager.getDatas();
+      console.log('Home -> res', res);
+    };
+
+    fetch();
+  }, []);
   return (
     <>
       <h3>Hello world !</h3>
---
+++ src/services/APIManager/index.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'https://newsapi.org/'
+});
+
+API.interceptors.request.use(
+  ({ headers, ...config }) => ({
+    ...config,
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+      Authorization: process.env.NEXT_PUBLIC_NEWS_API_KEY
+    }
+  }),
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default class APIManager {
+  static async getDatas() {
+    const res = await API.get('v2/top-headlines?category=science');
+    return res.data;
+  }
+}
