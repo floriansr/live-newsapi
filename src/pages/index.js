4b8d837bc5172fec5a4312e50bc2056bb57e7cb4
floriansr <sueurfloriansf@gmail.com>
2020-12-16 10:39:49 +0100

    [add] gridContainer styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,25 @@ import * as React from 'react';
 import { useEffect, useState } from 'react';
 
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
 import shortid from 'shortid';
 
 import APIManager from 'services/APIManager';
 
 import MediaCard from 'components/MediaCard';
 
+const useStyles = makeStyles(() => ({
+  gridContainer: {
+    height: '100vh',
+    padding: '0 2rem'
+  }
+}));
+
+
 const Home = () => {
+    const classes = useStyles();
+
   const [news, setNews] = useState([]);
   useEffect(() => {
     const fetchNews = async () => {
@@ -21,7 +33,11 @@ const Home = () => {
   }, []);
   return (
     <>
-      <Grid container spacing={3} alignItems="center">
+      <Grid
+        container
+        spacing={3}
+        alignItems="center"
+        className={classes.gridContainer}>
         {news.length !== 0 &&
           news.map((article) => (
             <Grid item xs key={shortid.generate()}>
